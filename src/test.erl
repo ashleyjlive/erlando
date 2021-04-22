8b5fd38151049ba34ec3297747bbabb9aa2edd20
ASHLEYDESKTOP\Ashley <ashley@parallaxnetworks.com>
2021-04-22 18:06:43 +1000

    Compilation Errors - OTP 23: • Don't error for warnings. • Upgrade stacktrace retrieval.

--- src/test.erl
+++ src/test.erl
@@ -76,6 +76,6 @@ hoist(ErrorT, Label, PlainFun) ->
                PlainFun(),
                return(passed)
            catch
-               Class:Reason ->
-                   fail({Label, Class, Reason, erlang:get_stacktrace()})
+               Class:Reason:Stck ->
+                   fail({Label, Class, Reason, Stck})
            end]).
